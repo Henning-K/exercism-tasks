f32523d341f0877489acf3f39a46962c9ed895da
Lev1a <henning.kowalk962@web.de>
2017-05-01 17:52:17 +0200

    intermediate progress on forth exercise

--- forth/src/lib.rs
+++ forth/src/lib.rs
@@ -1,7 +1,9 @@
 pub type Value = i32;
 pub type ForthResult = Result<(), Error>;
 
-pub struct Forth;
+pub struct Forth {
+    stack: Vec<isize>,
+}
 
 #[derive(Debug, PartialEq)]
 pub enum Error {
@@ -11,13 +13,42 @@ pub enum Error {
     InvalidWord,
 }
 
+use std::str::FromStr;
+
+enum Token {
+    Num(isize),
+    Arith(&str),
+    Mani(&str),
+}
+
 impl Forth {
     pub fn new() -> Forth {
+        Forth { stack: vec![] }
     }
 
     pub fn format_stack(&self) -> String {
+        let mut result = String::new();
+        if self.stack.is_empty() {
+            return result;
+        }
+
+
+
+        result
     }
 
     pub fn eval(&mut self, input: &str) -> ForthResult {
+        let tokens: Vec<&str> = input.split(|x| !(x.is_alphabetic() ||
+                                                  x.is_numeric() ||
+                                                  "+*/-".contains(x))).collect();
+        for tok in tokens {
+            let tok = match isize::from_str(tok) {
+                Ok(n) => Token::Num(n),
+                Err(_) => {
+                     
+                },
+            };
+        }
+        Ok(())
     }
 }
--- forth/tests/forth.rs
+++ forth/tests/forth.rs
@@ -10,7 +10,7 @@ fn no_input_no_stack() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn numbers_just_get_pushed_onto_the_stack() {
     let mut f = Forth::new();
     f.eval("1 2 3 4 5 -1");
