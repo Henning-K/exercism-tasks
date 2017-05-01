3a4a4e4aa9d5e2c7a6326ce8fb7439607135a38b
Henning-K <henning.kowalk962@web.de>
2017-05-01 23:45:28 +0200

    progress on forth exercise

--- forth/src/lib.rs
+++ forth/src/lib.rs
@@ -40,12 +40,73 @@ impl Forth {
     pub fn eval(&mut self, input: &str) -> ForthResult {
         let tokens: Vec<&str> = input.split(|x| !(x.is_alphabetic() ||
                                                   x.is_numeric() ||
-                                                  "+*/-".contains(x))).collect();
+                                                  "+*/-".contains(x)))
+                                                  .collect();
         for tok in tokens {
             let tok = match isize::from_str(tok) {
                 Ok(n) => Token::Num(n),
                 Err(_) => {
-                     
+                    let tok: String = tok.to_lowercase().collect();
+                    match &tok {
+                        "+" => {
+                            let rhs = match self.stack.pop() {
+                                None => return Err(Error::StackUnderflow),
+                                Some(r) => r,
+                            };
+                            let lhs = match self.stack.pop() {
+                                None => return Err(Error::StackUnderflow),
+                                Some(l) => l,
+                            };
+                            self.stack.push(lhs + rhs);
+                        },
+                        "-" => {
+                            let rhs = match self.stack.pop() {
+                                None => return Err(Error::StackUnderflow),
+                                Some(r) => r,
+                            };
+                            let lhs = match self.stack.pop() {
+                                None => return Err(Error::StackUnderflow),
+                                Some(l) => l,
+                            };
+                            self.stack.push(lhs - rhs);
+                        },
+                        "*" => {
+                            let rhs = match self.stack.pop() {
+                                None => return Err(Error::StackUnderflow),
+                                Some(r) => r,
+                            };
+                            let lhs = match self.stack.pop() {
+                                None => return Err(Error::StackUnderflow),
+                                Some(l) => l,
+                            };
+                            self.stack.push(lhs * rhs);
+                        },
+                        "/" => {
+                            let rhs = match self.stack.pop() {
+                                None => return Err(Error::StackUnderflow),
+                                Some(r) => r,
+                            };
+                            let lhs = match self.stack.pop() {
+                                None => return Err(Error::StackUnderflow),
+                                Some(l) => l,
+                            };
+                            if rhs == 0 {
+                                return Err(Error::DivisionByZero);
+                            }
+                            self.stack.push(lhs / rhs);
+                        },
+                        "dup" => {
+                            let rhs = match self.stack.pop() {
+                                None => return Err(Error::StackUnderflow),
+                                Some(r) => r,
+                            };
+                            self.stack.push(rhs);
+                            self.stack.push(rhs);
+                        },
+                        "drop" => {},
+                        "swap" => {},
+                        "over" => {},
+                    }
                 },
             };
         }
